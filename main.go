8f26df752bc8746b1173ff9750bd323cdaed994c
Phumphathai Chansriwong <ouoam2555@gmail.com>
2018-11-12 22:35:10 +0700

    Test Change

--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+type mydata struct {
+	Age      int
+	Name     string
+	Lastname string
+}
+
+type myTime struct {
+	ISO       time.Time
+	Stamp     int64
+	StampNano int64
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -18,18 +31,23 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+		t := time.Now()
 
-	router.GET("/mark", func(c *gin.Context) {
-		//mapD := map[string]string{"name": "Phumphathai", "lastname": "Chansriwong"}
-		//mapB, _ := json.Marshal(mapD)
-		//c.String(http.StatusOK, string(mapB))
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
+			"Me": &mydata{
+				Age:      19,
+				Name:     "Phumphathai",
+				Lastname: "Chansriwong",
+			},
+
+			"Time": &myTime{
+				ISO:       t,
+				Stamp:     t.Unix(),
+				StampNano: t.UnixNano(),
+			},
+
 			"Request.Header": c.Request.Header,
 		})
 	})
